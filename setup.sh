875e8726241fc63fd820052a8da5043ed66f6890
vvakame <vvakame+dev@gmail.com>
2015-11-14 16:32:47 +0900

    試しに build-in-docker.sh を追加してみた

--- setup.sh
+++ setup.sh
@@ -2,8 +2,12 @@
 
 set -eux
 
-rm -rf node_modules
+which grunt > /dev/null 2>&1
+if [ $? -ne 0 ] ; then
+	npm install -g grunt-cli
+fi
 
+rm -rf node_modules
 npm install
 
 curl -L https://github.com/vvakame/prh/raw/master/misc/techbooster.yml > articles/prh.yml
