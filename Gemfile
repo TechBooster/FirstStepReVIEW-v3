cd77041e49c8a124e5479162b42d46a53010408a
vvakame <vvakame+dev@gmail.com>
2015-11-23 16:36:32 +0900

    Node.js 4系ベースでGruntfile.jsを書き直した

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+gem 'review', '1.7.0'
 gem 'review-peg', '0.2.0'
--- Gruntfile.js
+++ Gruntfile.js
@@ -1,98 +1,97 @@
-module.exports = function (grunt) {
+"use strict";
+
+let fs = require("fs");
+let yaml = require("js-yaml");
+
+const articles = "articles";
+const bookConfig = yaml.safeLoad(fs.readFileSync(`${articles}/config.yml`, "utf8"));
+
+const reviewPrefix = process.env["REVIEW_PREFIX"] || "bundle exec ";
+const reviewPostfix = process.env["REVIEW_POSTFIX"] || "-peg";             // REVIEW_POSTFIX=" " grunt pdf とかすると通常版でビルドできるよ
+const reviewPreproc = `${reviewPrefix}review-preproc${reviewPostfix}`;
+const reviewCompile = `${reviewPrefix}review-compile${reviewPostfix}`;
+const reviewPdfMaker = `${reviewPrefix}review-pdfmaker${reviewPostfix}`;
+const reviewEpubMaker = `${reviewPrefix}review-epubmaker${reviewPostfix}`;
+
+module.exports = grunt => {
 	grunt.initConfig({
 		clean: {
 			review: {
 				src: [
-					'articles/C89-FirstStepReVIEW-v2-pdf/',
-					'articles/*.pdf',
-					'articles/*.epub',
-					'articles/*.html',
-					'articles/*.xml',
-					'articles/*.txt'
+					`${articles}/${bookConfig.bookname}-pdf/`,
+					`${articles}/*.pdf`,
+					`${articles}/*.epub`,
+					`${articles}/*.html`,
+					`${articles}/*.xml`,
+					`${articles}/*.txt`
 				]
 			}
 		},
 		exec: {
 			preprocess: {
-				cwd: "./articles",
-				cmd: function () {
-					var command = "bundle exec review-preproc-peg";
-					var files = [
-						"articles"
-					];
-					var exec = command + " -r --tabwidth=2 *.re";
-					console.log(exec);
-					return exec;
-				}
+				cwd: articles,
+				cmd: `${reviewPreproc} -r --tabwidth=2 *.re`
 			},
 			compile2text: {
-				cwd: "./articles",
-				cmd: function () {
-					return "bundle exec review-compile-peg --all --target=text";
-				}
+				cwd: articles,
+				cmd: `${reviewCompile} --all --target=text`
 			},
 			compile2html: {
-				cwd: "./articles",
-				cmd: function () {
-					return "bundle exec review-compile-peg --all --target=html --stylesheet=style.css --chapterlink";
-				}
+				cwd: articles,
+				cmd: `${reviewCompile} --all --target=html --stylesheet=style.css --chapterlink`
 			},
 			compile2latex: {
-				cwd: "./articles",
-				cmd: function () {
-					return "bundle exec review-compile-peg --all --target=latex --footnotetext";
-				}
+				cwd: articles,
+				cmd: `${reviewCompile} --all --target=latex --footnotetext`
 			},
 			compile2idgxml: {
-				cwd: "./articles",
-				cmd: function () {
-					return "bundle exec review-compile-peg --all --target=idgxml";
-				}
+				cwd: articles,
+				cmd: `${reviewCompile} --all --target=idgxml`
 			},
 			compile2pdf: {
-				cwd: "./articles",
-				cmd: function () {
-					return "bundle exec review-pdfmaker-peg config.yml";
-				}
+				cwd: articles,
+				cmd: `${reviewPdfMaker} config.yml`
 			},
 			compile2epub: {
-				cwd: "./articles",
-				cmd: function () {
-					return "bundle exec review-epubmaker-peg config.yml";
-				}
+				cwd: articles,
+				cmd: `${reviewEpubMaker} config.yml`
 			}
 		}
 	});
 
-	function generateTask(target, pretask) {
-		pretask = pretask || [];
-		return ['clean', 'exec:preprocess', 'exec:compile2' + target];
+	function generateTask(target) {
+		return ["clean", "exec:preprocess", `exec:compile2${target}`];
 	}
 
 	grunt.registerTask(
-		'default',
+		"default",
 		"原稿をコンパイルしてPDFファイルにする",
 		"pdf");
 
 	grunt.registerTask(
-		'text',
+		"text",
 		"原稿をコンパイルしてTextファイルにする",
 		generateTask("text"));
 
 	grunt.registerTask(
-		'html',
+		"html",
 		"原稿をコンパイルしてHTMLファイルにする",
 		generateTask("html"));
 
 	grunt.registerTask(
-		'idgxml',
+		"idgxml",
 		"原稿をコンパイルしてInDesign用XMLファイルにする",
 		generateTask("idgxml"));
 
 	grunt.registerTask(
-		'pdf',
+		"pdf",
 		"原稿をコンパイルしてpdfファイルにする",
 		generateTask("pdf"));
+
+	grunt.registerTask(
+		"epub",
+		"原稿をコンパイルしてepubファイルにする",
+		generateTask("epub"));
 
 	require('load-grunt-tasks')(grunt);
 };
