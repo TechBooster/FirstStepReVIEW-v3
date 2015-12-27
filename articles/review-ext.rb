a0e99582e90324c1663d1fcfaa02908c3b30cecf
mstssk <mstssk+github@gmail.com>
2015-12-27 17:43:43 +0900

    htmlのtableにbootstrap用のclassつける

---
+++ articles/review-ext.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+module ReVIEW
+	class HTMLBuilder
+		def table_begin(ncols)
+			# Bootstrapのtable用classをつける
+			puts '<table class="table table-bordered table-striped">'
+		end
+	end
+end
