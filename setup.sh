322dd0b2dcb2bc2f02c58bf99550bf2a0566d692
vvakame <vvakame+dev@gmail.com>
2016-01-27 02:56:49 +0900

    npm install時にpostinstall hookがroot(= docker)環境で動作していなかったようなので修正 fixes #26

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 set -eux
 
 rm -rf node_modules
-npm install
+# --unsafe-perm はrootでの実行時(= docker環境)で必要 非root時の挙動に影響なし
+npm install --unsafe-perm
 
 curl -L https://github.com/vvakame/prh/raw/master/misc/techbooster.yml > articles/prh.yml
