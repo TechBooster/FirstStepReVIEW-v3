af5b4c5f7e684910b00a98933688a012329044b1
p <_>
2018-09-25 11:54:02 +0900

    setup.sh を初回だけ実行するようにする

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+if ! (test -e node_modules && test -e vendor/bundle); then
+  docker run -t --rm -v $(pwd):/book vvakame/review:2.4 /bin/bash -ci "cd /book && ./setup.sh"
+fi
+
 # コマンド手打ちで作業したい時は以下の通り /book に pwd がマウントされます
 # docker run -i -t -v $(pwd):/book vvakame/review /bin/bash
 
-docker run -t --rm -v $(pwd):/book vvakame/review /bin/bash -ci "cd /book && ./setup.sh && npm run pdf"
+docker run -t --rm -v $(pwd):/book vvakame/review /bin/bash -ci "cd /book && npm run pdf"
