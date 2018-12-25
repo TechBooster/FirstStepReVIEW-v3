3105caa37fa45174b1d235f164b43de35efde583
vvakame <vvakame+dev@gmail.com>
2018-12-25 12:40:56 +0900

    ちょっと手直し

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if ! (test -e node_modules && test -e vendor/bundle); then
-  docker run -t --rm -v $(pwd):/book vvakame/review:2.4 /bin/bash -ci "cd /book && ./setup.sh"
+  # node_modules と vendor/bundle がなかったらsetup.shを動かして作成する
+  # ホスト環境でこれを作成していた場合、噛み合わせが悪い場合があるので手動で消してやり直す必要がある
+  docker run -t --rm -v $(pwd):/book vvakame/review /bin/bash -ci "cd /book && ./setup.sh"
 fi
 
 # コマンド手打ちで作業したい時は以下の通り /book に pwd がマウントされます
